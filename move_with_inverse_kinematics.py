1c7ea82651b1ec2019ea683182ae886a33294e48
Guillermo Barbadillo <guillermobarbadillo@gmail.com>
2020-06-16 17:52:38 +0200

    allow to pass urdf model as input

--- move_with_inverse_kinematics.py
+++ move_with_inverse_kinematics.py
@@ -1,31 +1,44 @@
+import sys
+import argparse
 import pybullet
 import pybullet_data
 
-pybullet.connect(pybullet.GUI)
-pybullet.setAdditionalSearchPath(pybullet_data.getDataPath())
-plane = pybullet.loadURDF("plane.urdf")
-robot = pybullet.loadURDF("../kuka_experimental/kuka_lbr_iiwa_support/urdf/lbr_iiwa_14_r820.urdf",
-                          [0, 0, 0], useFixedBase=1)  # use a fixed base!
-pybullet.setGravity(0, 0, -9.81)
-pybullet.setRealTimeSimulation(1) #this makes the simulation real time
+def main():
+    args = parse_args()
+    pybullet.connect(pybullet.GUI)
+    pybullet.setAdditionalSearchPath(pybullet_data.getDataPath())
+    plane = pybullet.loadURDF("plane.urdf")
+    robot = pybullet.loadURDF(args.robot_path, [0, 0, 0], useFixedBase=1)  # use a fixed base!
+    pybullet.setGravity(0, 0, -9.81)
+    pybullet.setRealTimeSimulation(1) #this makes the simulation real time
 
-position_idx = []
-position_idx.append(pybullet.addUserDebugParameter('x', -2, 2, 0.1))
-position_idx.append(pybullet.addUserDebugParameter('y', -2, 2, 0.1))
-position_idx.append(pybullet.addUserDebugParameter('z', 0.2, 2, 1))
+    position_idx = []
+    position_idx.append(pybullet.addUserDebugParameter('x', -2, 2, 0.1))
+    position_idx.append(pybullet.addUserDebugParameter('y', -2, 2, 0.1))
+    position_idx.append(pybullet.addUserDebugParameter('z', 0.2, 2, 1))
 
-orientation_idx = []
-orientation_idx.append(pybullet.addUserDebugParameter('euler 1', -3.14, 3.14, 0))
-orientation_idx.append(pybullet.addUserDebugParameter('euler 2', -3.14, 3.14, 0))
-orientation_idx.append(pybullet.addUserDebugParameter('euler 2', -3.14, 3.14, 0))
+    orientation_idx = []
+    orientation_idx.append(pybullet.addUserDebugParameter('euler 1', -3.14, 3.14, 0))
+    orientation_idx.append(pybullet.addUserDebugParameter('euler 2', -3.14, 3.14, 0))
+    orientation_idx.append(pybullet.addUserDebugParameter('euler 2', -3.14, 3.14, 0))
 
+    n_joints = pybullet.getNumJoints(robot)
+    print('The number of joints in the robot is: %i' % n_joints)
+    while 1:
+        euler = [pybullet.readUserDebugParameter(idx) for idx in orientation_idx]
+        orientation = pybullet.getQuaternionFromEuler(euler)
+        position = [pybullet.readUserDebugParameter(idx) for idx in position_idx]
+        target_positions = pybullet.calculateInverseKinematics(robot, n_joints-2, position, orientation)
+        pybullet.setJointMotorControlArray(robot, range(len(target_positions)), pybullet.POSITION_CONTROL,
+                                           targetPositions=target_positions)
 
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Allows to move each joint of the robot independently.',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-r', '--robot_path', help='Path to the urdf model of the robot',
+                        default='../kuka_experimental/kuka_lbr_iiwa_support/urdf/lbr_iiwa_14_r820.urdf')
+    return parser.parse_args(sys.argv[1:])
 
-while 1:
-    euler = [pybullet.readUserDebugParameter(idx) for idx in orientation_idx]
-    orientation = pybullet.getQuaternionFromEuler(euler)
-    position = [pybullet.readUserDebugParameter(idx) for idx in position_idx]
-    target_positions = pybullet.calculateInverseKinematics(robot, 7, position, orientation)
-    # target_positions = pybullet.calculateInverseKinematics(robot, 7, position)
-    pybullet.setJointMotorControlArray(robot, range(7), pybullet.POSITION_CONTROL,
-                                       targetPositions=target_positions)
+if __name__ == '__main__':
+    main()
